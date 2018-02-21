1c763b28bbed7404fd6bfa6503f71d657ce54d12
Mark Shirley <mark.h.shirley@nasa.gov>
2018-02-21 12:12:49 -0800

    enable cross origin proxy; needed for react-create-app proxy

--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -3,6 +3,8 @@ import PacketSubset from './PacketSubset.js';
 import Client from './Client.js';
 import Draggable from 'react-draggable';
 
+const websocket_url = 'ws:localhost:8000/realtime';
+
 class WarpDesktop extends React.Component {
   constructor(props) {
     super(props);
@@ -16,18 +18,41 @@ class WarpDesktop extends React.Component {
     var self = this;
     Client.get_dictionary('/root', function(dict) {
       var names = dict.packets.map(p => p.id);
-      names = names.filter(n => !n.includes("TABLE") && !n.includes("TBL"));
+      names = names.filter(n => !n.includes('TABLE') && !n.includes('TBL'));
       names.sort();
       self.setState((prevState, props) => {
         return { dictionary: dict, packetNames: names };
       });
+      if ('WebSocket' in window) {
+        var socket = new WebSocket(websocket_url);
+        self.socket = socket;
+        socket.onopen = function() {
+          self.sendRealtime({ request: 'ping', token: '1' });
+        };
+        socket.onmessage = function(evt) {
+          console.log('got evt=', evt);
+        };
+        socket.onclose = function() {};
+        window.onbeforeunload = function(event) {
+          socket.close();
+        };
+      }
     });
+  }
+  sendRealtime(msg) {
+    if (this.socket === undefined) {
+      console.log("No websocket.  Can't send ", msg);
+    } else {
+      this.socket.send(JSON.stringify(msg));
+    }
   }
   render() {
     return (
       <div className="warpDesktop">
         <PacketDock>
-          {this.state.packetNames.map(n => <PacketGenerator key={n} name={n} />)}
+          {this.state.packetNames.map(n => (
+            <PacketGenerator key={n} name={n} />
+          ))}
         </PacketDock>
         <WarpField />
       </div>
@@ -50,7 +75,7 @@ class PacketGenerator extends React.Component {
   render() {
     return (
       <Draggable>
-      <div className="packetGenerator cursor">{this.props.name}</div>
+        <div className="packetGenerator cursor">{this.props.name}</div>
       </Draggable>
     );
   }
